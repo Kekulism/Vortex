fdc3c41b3858de2207c43b154394c75b57b99631
Kekulism <33201764+Kekulism@users.noreply.github.com>
2025-04-23 12:45:52 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,17 @@
+SMODS.Atlas({
+	key = "modicon",
+	path = "kvort_icon.png",
+	px = 32,
+	py = 32
+})
+
+SMODS.Shader {
+	key = 'splash',
+	path = 'splash.fs',
+	prefix_config = {key = false}
+}
+SMODS.Shader {
+	key = 'background',
+	path = 'background.fs',
+	prefix_config = {key = false}
+}
